3ebb03649fdd9ecce3d9e407aa80944a0762c044
gerardo anguiano <star-0102@hotmail.com>
2021-03-02 18:56:03 -0800

    se creo una practica para calcular el perimetro de un triangulo

---
+++ Practica_2/CalculoPerimetro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar tres variables de tipo int y asignarles valore estaticos
+            int a = 10;
+            int b = 10;
+            int c = 25;
+            //2 crear una variable de tipo int la cual guardara la suma de las tres variables
+            int suma = a + b + c;
+
+            //3 mostrar en pantalla el valor de suma "el valor de suma es"
+            Console.WriteLine("El perimetro del triangulo es " + suma);
+            Console.WriteLine("presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
