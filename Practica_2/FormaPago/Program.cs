6173d72e68abd14d09fafdf561e0d6d3b0dae399
gerardo anguiano <star-0102@hotmail.com>
2021-03-18 20:03:38 -0700

    Practica  solicitar un caracter y detectar si es letra minuscula, letra mayuscula  o numero

---
+++ Practica_2/FormaPago/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormaPago
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir precio del producto 
+            Console.WriteLine("escriba precio del producto");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+            //pedir forma de pago con tarjeta o en efectivo 
+            Console.WriteLine("escriba la forma de pago tarjeta o efectivo");
+            string formapago = (Console.ReadLine()).ToLower();
+
+            //en caso de ser tarjeta pedir el numero de tarjeta 
+            if(formapago.Equals("tarjeta"))
+            {
+                Console.WriteLine("introduce el numero de tarjeta");
+                int numero_tarjeta = Convert.ToInt32(Console.ReadLine());
+
+            }
+            //imprimir mensaje el producto con precio
+            Console.WriteLine("el producto con precio"+ precio + "se ha pagado con el; numero de tarjeta"+  )
+            //imprimir mensaje 
+        }
+    }
+}
---
+++ Practica_2/Ifchar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ifchar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1 pedir por consola el caracter
+            Console.WriteLine("Por favor escribe un caracter");
+            char c = (char)Console.Read();
+            if (char.IsLetter(c))
+            {
+                if (char.IsLower(c))
+                {
+                    Console.WriteLine("el caracter es letra minuscula");
+                }
+                else
+                {
+                    Console.WriteLine("el caracter es letra mayuscula");
+                }
+            }
+            else
+            {
+                Console.WriteLine("el caracter es numero");
+            }
+            Console.WriteLine("presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+
+        
+
